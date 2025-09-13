5b734c60cbc88563fafb6d36d2451d740abda97a
SIDDHU-1944 <bairaboinasiddhartha@gmail.com>
2025-09-13 12:22:07 +0530

    correction of file name importing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/ContactMe/ContactMe";
 import ParticlesBackground from "./components/ParticlesBack/ParticlesBackgr.jsx";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer.jsx";
 function App() {
   return (
     <div className={styles.App}>
